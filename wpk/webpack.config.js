c6da0cac1f1c50cb3cc8f2b98f8525935f8abd1c
luodaiyu <luodaiyuanacy@163.com>
2019-05-31 10:56:48 +0800

    抽离css样式到单独文件，并对打包后的js,css进行压缩

--- wpk/webpack.config.js
+++ wpk/webpack.config.js
@@ -1,6 +1,9 @@
 //wenpack是使用node写出来的
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');//抽离css到单独的文件
+const TerserJSPlugin = require("terser-webpack-plugin");//压缩优化js代码
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");//压缩css代码
 module.exports = {
     devServer: {//开发服务器配置 
         port: '3000',
@@ -15,16 +18,30 @@ module.exports = {
         path: path.resolve(__dirname, 'build')//路径必须是一个绝对路径
 
     },
+    optimization: {//优化项
+        minimizer: [
+            new TerserJSPlugin({
+                cache:true,//启用文件缓存。缓存目录的默认路径：node_modules/.cache/terser-webpack-plugin
+                parallel:true,//使用多进程并行运行来提高构建速度 强烈建议
+                sourceMap:true//源码映射
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({//根据模版文件自动生成一个引入bundle.js的html文件
             template: './src/index.html',//以谁为模版
             filename: 'index.html ',//打包出来的文件名
-            minify: {//最小化文件，压缩代码
-                removeAttributeQuotes: true,//去掉双引号
-                collapseWhitespace: true,//变为一行
-            },
-            hash: true//打包后的文件加上hash挫
-        })
+            // minify: {//最小化文件，压缩代码
+            //     removeAttributeQuotes: true,//去掉双引号
+            //     collapseWhitespace: true,//变为一行
+            // },
+            // hash: true//打包后的文件加上hash挫
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+
     ],
     module: {//模块
         rules: [//解析以.css结尾的文件，根据特定的loader
@@ -34,29 +51,20 @@ module.exports = {
             // { test: /\.css$/, use:['style-loader','css-loader']},
             //loader还可以写成对象形式。
             {
-                test: /\.css$/, use: [
-                    {
-                        loader: 'style-loader',
-                        options: {//将style插入到顶部。为了使我们自己在html里写的样式优先级更高
-                            insertAt: 'top'
-                        }
-                    },
-                    {
-                        loader: 'css-loader'
-                    }
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,//抽离出来的文件以link方式插入html
+                    'css-loader',
+                    'postcss-loader',//css前面自动加前缀
                 ]
             },
             //处理less文件
             {
-                test: /\.less$/, 
+                test: /\.less$/,
                 use: [
-                    {
-                        loader: 'style-loader',
-                        options: {//将style插入到顶部。为了使我们自己在html里写的样式优先级更高
-                            insertAt: 'top'
-                        }
-                    },
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
+                    'postcss-loader',
                     'less-loader',//less转为css
                 ]
             },
